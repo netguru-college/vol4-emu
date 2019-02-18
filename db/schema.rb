784d5ac1fa50ec8c01568026a8613e81db5f55e4
martom87 <marcintomala87@gmail.com>
2019-02-18 13:24:32 +0100

    add_sport

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_18_105047) do
+ActiveRecord::Schema.define(version: 2019_02_18_121955) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "categories", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
 
   create_table "events", force: :cascade do |t|
     t.string "title"
@@ -29,6 +23,10 @@ ActiveRecord::Schema.define(version: 2019_02_18_105047) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "sports", force: :cascade do |t|
+    t.string "name"
   end
 
   create_table "users", force: :cascade do |t|
