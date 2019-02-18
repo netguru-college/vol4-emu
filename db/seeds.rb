39e684678d81089bc8706af48ec6f94074ed602d
jells123 <julka.bedziechowska@gmail.com>
2019-02-18 16:08:26 +0100

    Add short sport seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,19 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+sports_names_list = [
+    'Soccer',
+    'Cricket',
+    'Field Hockey',
+    'Tennis',
+    'Volleyball',
+    'Table Tennis',
+    'Basketball', 
+    'Baseball',
+    'Rugby',
+    'Golf'
+]
+
+sports_names_list.each do |name|
+    Sport.create(name: name)
+end
