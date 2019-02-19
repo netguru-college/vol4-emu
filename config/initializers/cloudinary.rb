f5762068d4799385a4bbc9d303c3587d64e58a75
tojawawel <bgawel93@gmail.com>
2019-02-19 12:46:28 +0100

    fixing cloudinary config

--- config/initializers/cloudinary.rb
+++ config/initializers/cloudinary.rb
@@ -1,5 +1,5 @@
 Cloudinary.config do |config|
   config.cloud_name = Rails.application.credentials.public_send((Rails.env + "_cloudinary").to_sym)[:cloud_name]
-  config.api_key = Rails.application.credentials.public_send((Rails.env + "_cloudinary").to_sym)[:api_key
+  config.api_key = Rails.application.credentials.public_send((Rails.env + "_cloudinary").to_sym)[:api_key]
   config.api_secret = Rails.application.credentials.public_send((Rails.env + "_cloudinary").to_sym)[:api_secret]
 end
