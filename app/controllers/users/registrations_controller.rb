52631e9c1c13667bff23b2f11f61f91210bdf09e
jluczak <jluczaksas@gmail.com>
2019-02-19 14:26:34 +0100

    Sign up permissions

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -7,11 +7,17 @@ class Users::RegistrationsController < Devise::RegistrationsController
     devise_parameter_sanitizer.permit(
        :account_update,
         keys: [
-          :email, :username, :password, :password_confirmation, :current_password, 
+          :email, :username, :password, :password_confirmation, :current_password,
           :avatar, :description, :city,
-          skills_attributes: [:id, :level, :sport_id, :_destroy] 
+          skills_attributes: [:id, :level, :sport_id, :_destroy]
         ]
       )
-  end  
+      devise_parameter_sanitizer.permit(
+         :sign_up,
+          keys: [
+            :email, :username, :password, :password_confirmation
+          ]
+        )
+  end
 
 end
