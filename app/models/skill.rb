ae8a83c54765e73932a13de1d91bdd2fbb95a6a6
jells123 <julka.bedziechowska@gmail.com>
2019-02-19 12:17:45 +0100

    Add Level enum

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -1,4 +1,13 @@
 class Skill < ApplicationRecord
   belongs_to :user
   belongs_to :sport
+
+  enum level:
+  [
+    :never_tried_this_before,
+    :beginner,
+    :intermediate,
+    :advanced,
+    :champion  
+  ]
 end
