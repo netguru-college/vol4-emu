472c128ba26acd3d954e0687cec43a43bfb981da
jells123 <julka.bedziechowska@gmail.com>
2019-02-20 17:55:17 +0100

    Fix capacity/date not present

--- app/validators/start_date_before_end_date_validator.rb
+++ app/validators/start_date_before_end_date_validator.rb
@@ -1,9 +1,9 @@
 class StartDateBeforeEndDateValidator < ActiveModel::EachValidator
 
   def validate_each(record, attribute, value)
+    return if record.started_at.blank? || record.ended_at.blank?
     the_end = record.ended_at
     the_start = record.started_at
-
 
     unless the_end > the_start
       record.errors.add attribute, (options[:message] || " cannot be before #{the_end.strftime("%d/%m/%y/%H:%M")}!")
