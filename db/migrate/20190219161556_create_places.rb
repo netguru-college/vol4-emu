bbdd64ac668442e1e7316f8e3face7d34178253c
jells123 <julka.bedziechowska@gmail.com>
2019-02-20 09:31:49 +0100

    Fix icon + add indices to places

--- db/migrate/20190219161556_create_places.rb
+++ db/migrate/20190219161556_create_places.rb
@@ -1,13 +1,16 @@
 class CreatePlaces < ActiveRecord::Migration[5.2]
   def change
     create_table :places do |t|
-      t.string :name
-      t.decimal :latitude
-      t.decimal :longitude
+      t.string :name, null: false
+      t.decimal :latitude, null: false
+      t.decimal :longitude, null: false
       t.string :owner
       t.string :phone_number
 
       t.timestamps
+
+      add_index :latitude, :longitude, unique: true
+      add_index :name     
     end
   end
 end
