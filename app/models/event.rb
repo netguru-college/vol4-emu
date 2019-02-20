1ceafd67d9bc1d7c2fd22f9e8cb890c90dc02333
jluczak <jluczaksas@gmail.com>
2019-02-20 12:32:32 +0100

    delete event, hide create event

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,7 +1,7 @@
 class Event < ApplicationRecord
-  has_many :participations
+  has_many :participations, dependent: :destroy
   has_many :users, through: :participations
-  has_many :comments
+  has_many :comments, dependent: :destroy
 
   belongs_to :sport
   accepts_nested_attributes_for :sport
