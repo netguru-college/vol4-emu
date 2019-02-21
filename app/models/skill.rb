d0c3c55e87d68487a6dcaef74e25bb9670755bdf
jells123 <julka.bedziechowska@gmail.com>
2019-02-21 11:00:02 +0100

    Change to user_id scoped to sport_id

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -2,7 +2,8 @@ class Skill < ApplicationRecord
   belongs_to :user
   belongs_to :sport
 
-  validates_uniqueness_of :sport, scope: :user
+  # validates_uniqueness_of :sport, scope: :user
+  validates_uniqueness_of :user_id, scope: :sport_id
 
   enum level:
   [
